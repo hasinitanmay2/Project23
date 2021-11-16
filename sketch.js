e64a49df24ca193d40a4f19799d559cf41d2e9a8
hasinitanmay2 <78693631+hasinitanmay2@users.noreply.github.com>
2021-11-16 16:53:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function draw() {
   background(0);
 
   packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.x
+  packageSprite.y= packageBody.position.y
 
 
   drawSprites();
